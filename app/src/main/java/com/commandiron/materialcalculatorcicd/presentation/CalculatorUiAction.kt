a78c57fcf3644a7680cdf2b8b51380bcbe3d4902
Emir Demirli <commandiron@gmail.com>
2022-09-20 10:33:25 +0300

    -Migrated Material2 To Material3 -Added CalculatorUiActions.kt

---
+++ app/src/main/java/com/commandiron/materialcalculatorcicd/presentation/CalculatorUiAction.kt
@@ -0,0 +1,18 @@
+package com.commandiron.materialcalculatorcicd.presentation
+
+import androidx.compose.runtime.Composable
+import com.commandiron.materialcalculatorcicd.domain.CalculatorAction
+
+data class CalculatorUiAction(
+    val text: String?,
+    val highlightLevel: HighlightLevel,
+    val action: CalculatorAction,
+    val content: @Composable () -> Unit = {}
+)
+
+sealed interface HighlightLevel {
+    object Neutral: HighlightLevel
+    object SemiHighlighted: HighlightLevel
+    object Highlighted: HighlightLevel
+    object StronglyHighlighted: HighlightLevel
+}
